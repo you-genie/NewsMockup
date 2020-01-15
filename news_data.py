280045c268cafe4f20282659198025a0fc67073d
krista2811 <krista2811@naver.com>
2020-01-15 07:17:57 +0000

    Ver3.1 Added encoding

--- news_data.py
+++ news_data.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-
+# -*- coding: utf-8 -*- 
 
 class Comment:
     def __init__(self, user, time, body):
--- news_run.py
+++ news_run.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from flask import Flask, render_template, request
 from news_data import news_lists, comment_lists
 
